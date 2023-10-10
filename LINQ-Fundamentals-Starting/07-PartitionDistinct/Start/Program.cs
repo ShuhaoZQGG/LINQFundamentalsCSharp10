02cc9c96b3699ea4d553168de6e00193211f1a3f
Shuhao Zhang <shuhaozhangchris@gmail.com>
2023-10-10 15:36:25 -0400

    learn how to partition data in linq

--- LINQ-Fundamentals-Starting/07-PartitionDistinct/Start/Program.cs
+++ LINQ-Fundamentals-Starting/07-PartitionDistinct/Start/Program.cs
@@ -4,7 +4,22 @@
 SamplesViewModel vm = new();
 
 // Call Sample Method
-var result = vm.TakeQuery();
+//var result = vm.TakeQuery();
+//var result = vm.TakeMethod();
+//var result = vm.TakeRangeQuery();
+//var result = vm.TakeRangeMethod();
+//var result = vm.TakeWhileQuery();
+//var result = vm.TakeWhileMethod();
+//var result = vm.SkipQuery();
+//var result = vm.SkipMethod();
+//var result = vm.SkipWhileQuery();
+//var result = vm.SkipWhileMethod();
+//var result = vm.DistinctQuery();
+//var result = vm.DistinctMethod();
+//var result = vm.DistinctByQuery();
+//var result = vm.DistinctByMethod();
+//var result = vm.ChunkQuery();
+var result = vm.ChunkMethod();
 
 // Display Results
 vm.Display(result);
